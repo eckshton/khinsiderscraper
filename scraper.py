4ce81790deb72fbd0e8dffbe3c8ea43932a6ea81
eckshton <59490779+eckshton@users.noreply.github.com>
2022-04-07 17:12:13 -0700

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,68 @@
+from pickle import FALSE, TRUE
+import requests
+import os
+
+url = input("Enter url: ")
+type = input("Type: ")
+
+sub = url.split("/")
+
+r = requests.get(url)
+text = r.text
+hrefs = []
+songnames = []
+
+while text.find('<a href="/' + sub[3] + '/album/' + sub[5] + '/') != -1:
+    start = text.find('<a href="/' + sub[3] + '/album/' + sub[5] + '/')
+    end = text.find('</a>', start)
+    hrefs.append(text[start:end])
+    text = text[end:]
+
+
+condensedhrefs = []
+
+for x in hrefs:
+    condensedhrefs.append(x.split('"')[1])
+
+uniqueurls = []
+
+for x in condensedhrefs:
+    alreadythere = FALSE
+    for y in uniqueurls:
+        if x == y:
+            alreadythere = TRUE
+            break
+    if alreadythere == FALSE:
+        uniqueurls.append(x)
+        songnames.append(hrefs[condensedhrefs.index(x)].split('">')[1])
+
+finalurls = []
+
+print('finalizing urls')
+
+if type=='flac':
+    for x in uniqueurls:
+        s = requests.get('https://downloads.khinsider.com' + x)
+        text2 = s.text
+        start = text2.find('href="https://vgmsite.com/soundtracks/' + sub[5] + '/')
+        end = text2.find('">',start)
+        start2 = text2.find('href="https://vgmsite.com/soundtracks/' + sub[5] + '/', end)
+        end2 = text2.find('">',start2)
+        finalurls.append(text2[start2+6:end2])
+if type=='mp3':
+    for x in uniqueurls:
+        s = requests.get('https://downloads.khinsider.com' + x)
+        text2 = s.text
+        start = text2.find('href="https://vgmsite.com/soundtracks/' + sub[5] + '/')
+        end = text2.find('">',start)
+        finalurls.append(text2[start+6:end])
+
+if os.path.isfile(sub[5]) == FALSE:
+    os.mkdir(sub[5])
+
+for x in finalurls:
+    print("getting " + songnames[finalurls.index(x)])
+    s = requests.get(x)
+    file = open(sub[5]+"/"+songnames[finalurls.index(x)]+"."+type,"wb")
+    file.write(s.content)
+    file.close()
